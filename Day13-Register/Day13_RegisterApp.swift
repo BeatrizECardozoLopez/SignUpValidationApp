4f0e0fd15fdf1d66e9bea2a705ee8d1379f756ba
BeatrizECardozoLopez <beatrizecardozolopez@gmail.com>
2024-08-15 09:27:30 -0400

    Initial Commit

---
+++ Day13-Register/Day13_RegisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day13_RegisterApp.swift
+//  Day13-Register
+//
+//  Created by Beatriz Cardozo on 15/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Day13_RegisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
